def1bc2ab08bd91e87997a0b7ca2ad7eb2f0da02
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-08-16 10:36:26 -0500

    fixed repo conf

--- active.products/src/main/java/ec/edu/espe/arquitectura/banquito/activeproducts/repository/LoanProductRepository.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.espe.arquitectura.banquito.activeproducts.repository;
-
-import ec.edu.espe.arquitectura.banquito.activeproducts.model.LoanProduct;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LoanProductRepository extends MongoRepository <LoanProduct, String> {
-
-
-}
