55d978de7b8ed835734ee7aa0d52e8175f4712c8
Daniel Alonso Gavilanes Falconi <dagavilanes2@espe.edu.ec>
2023-08-19 21:04:36 -0500

    Update Active Products PU

---
+++ src/test/java/ec/edu/espe/arquitectura/banquito/active/products/controller/LoanProductControllerTest.java
@@ -0,0 +1,24 @@
+package ec.edu.espe.arquitectura.banquito.active.products.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanProductControllerTest {
+
+    @Test
+    void listLoandProduct() {
+    }
+
+    @Test
+    void listLoandProductSelected() {
+    }
+
+    @Test
+    void findBySelected() {
+    }
+
+    @Test
+    void findByProductos() {
+    }
+}
