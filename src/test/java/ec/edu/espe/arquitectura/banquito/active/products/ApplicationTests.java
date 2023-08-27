318d2e32968de7d7b269e3bf52da739b55790270
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-08-27 15:35:15 -0500

    .m

--- src/test/java/ec/edu/espe/arquitectura/banquito/active/products/ApplicationTests.java
+++ src/test/java/ec/edu/espe/arquitectura/banquito/active/products/ApplicationTests.java
@@ -3,6 +3,7 @@ package ec.edu.espe.arquitectura.banquito.active.products;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class ApplicationTests {
 
