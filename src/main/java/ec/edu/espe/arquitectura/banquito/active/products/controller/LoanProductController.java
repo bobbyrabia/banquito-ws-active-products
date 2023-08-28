6106fc479087db5d3bbed60fd38d7995cfa93658
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-08-28 18:24:29 -0500

    fixed cors

--- src/main/java/ec/edu/espe/arquitectura/banquito/active/products/controller/LoanProductController.java
+++ src/main/java/ec/edu/espe/arquitectura/banquito/active/products/controller/LoanProductController.java
@@ -7,16 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/loanProduct")
+@CrossOrigin
 public class LoanProductController {
     private final LoanProductService service;
 
